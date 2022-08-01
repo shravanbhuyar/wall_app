ab99561f1cb9cc368663133560f4eebccbca38fe
shravanbhuyar <shravancbhuyar@gmail.com>
2022-08-01 20:29:28 +0530

    new download library

--- src/components/imageFull.js
+++ src/components/imageFull.js
@@ -12,36 +12,61 @@ import {
 } from 'react-native';
 import {scale} from 'react-native-size-matters';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
-import CameraRoll from '@react-native-community/cameraroll';
+import RNFetchBlob from 'rn-fetch-blob';
 
 const ImageFull = props => {
-  async function hasAndroidPermission() {
-    const permission = PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE;
-
-    const hasPermission = await PermissionsAndroid.check(permission);
-    if (hasPermission) {
-      return true;
+  const checkPermission = async () => {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
+        {
+          title: 'Storage Permission Required',
+          message: 'App needs access to your storage to download Images',
+        },
+      );
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        console.log('Storage Permission Granted.');
+        downloadImage();
+      } else {
+        alert('Storage Permission Not Granted');
+      }
+    } catch (err) {
+      console.warn(err);
     }
+  };
 
-    const status = await PermissionsAndroid.request(permission);
-    return status === 'granted';
-  }
+  const downloadImage = async () => {
+    let date = new Date();
+    let ext = getExtention(props.route.params.item.url);
+    ext = '.' + ext[0];
+    const {config, fs} = RNFetchBlob;
+    let PictureDir = fs.dirs.PictureDir;
 
-  async function savePicture(tag) {
-    if (Platform.OS === 'android' && !(await hasAndroidPermission())) {
-      return;
-    }
-
-    CameraRoll.save(
-      tag,
-      ('photo',
-      Environment.getExternalStoragePublicDirectory(
-        Environment.DIRECTORY_DOWNLOADS,
-      )),
-    )
-      .then(res => console.log(res))
+    let options = {
+      fileCache: true,
+      addAndroidDownloads: {
+        useDownloadManager: true,
+        notification: true,
+        path:
+          PictureDir +
+          '/image_' +
+          Math.floor(date.getTime() + date.getSeconds() / 2) +
+          ext,
+        description: 'Image',
+      },
+    };
+    config(options)
+      .fetch('GET', props.route.params.item.url)
+      .then(res => {
+        console.log('res -> ', JSON.stringify(res));
+        alert('Image Downloaded Successfully.');
+      })
       .catch(err => console.log(err));
-  }
+  };
+
+  const getExtention = filename => {
+    return /[.]/.exec(filename) ? /[^.]+$/.exec(filename) : undefined;
+  };
 
   return (
     <View>
@@ -80,7 +105,7 @@ const ImageFull = props => {
           <View style={styles.touchableView}>
             <TouchableNativeFeedback
               background={TouchableNativeFeedback.Ripple('black', true)}
-              onPress={() => savePicture(props.route.params.item.url)}>
+              onPress={checkPermission}>
               <View>
                 <Icon
                   name="download"
